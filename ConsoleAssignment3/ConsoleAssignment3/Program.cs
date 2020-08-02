ff6f3aa6e0cedec2eec8d36b3bfe08f0408f8b9d
dotnet-sandipan-july-2020 <68274705+dotnet-sandipan-july-2020@users.noreply.github.com>
2020-08-02 21:03:09 +0530

    assignment3

---
+++ ConsoleAssignment3/ConsoleAssignment3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryVehichle;
+
+namespace ConsoleAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Vehicle("BMW", "Black", 0, 4, 0);
+            Vehicle bus = new Vehicle("TATA", "Blue", 2, 4, 2);
+           
+            car.start();
+            car.Display();
+            car.stop();
+            Console.WriteLine("--------------BUS--------------");                
+            bus.start();
+            bus.Display();
+            bus.stop();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
