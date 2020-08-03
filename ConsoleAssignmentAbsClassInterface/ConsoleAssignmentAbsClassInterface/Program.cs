b1c3ce11fe894d946b03f0efa22e1c4aa64a6e61
dotnet-sandipan-july-2020 <68274705+dotnet-sandipan-july-2020@users.noreply.github.com>
2020-08-03 17:35:19 +0530

    ClassAssignment2_abstract&interfaces

---
+++ ConsoleAssignmentAbsClassInterface/ConsoleAssignmentAbsClassInterface/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAssignmentAbsClassInterface
+{
+    interface IVolume
+    {
+        double Volume { get; }
+    }
+    interface IArea
+    {
+        double SurfaceArea();
+
+    }
+    interface ISolid 
+    {
+         double  Volume { get; }
+          double SurfaceArea();
+    }
+
+    abstract class Solid : ISolid
+    {
+        public double radius;
+       public  const double pi = 3.142;
+       
+        
+
+
+        public abstract double Volume { get; }
+        public abstract double SurfaceArea();
+        public double Radius 
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public Solid(double radi)
+        {
+            radius = radi;
+
+        }
+    }
+
+    class Cylinder : Solid
+    {
+        public double Height;
+        
+        
+        public Cylinder(double _height,double radius):base(radius)
+        {
+            Height = _height;
+
+        }
+        public override double Volume
+        {
+            get { return pi * Radius * Radius * Height; }
+        }
+
+
+        public override double SurfaceArea()
+        {
+            var surfaceA = 2 * pi * Radius * (Radius + Height);
+            return surfaceA;
+           
+        }
+    }
+    class Sphere : Solid
+    {
+        public double Height;
+
+
+        public Sphere(double _height, double radius) : base(radius)
+        {
+            Height = _height;
+
+        }
+        public override double Volume
+        {
+            get { return 4 * pi * Radius * Radius; }
+        }
+
+
+        public override double SurfaceArea()
+        {
+            var surfaceA = (4 / 3) * pi * Radius * Radius * Radius;
+            return surfaceA;
+
+        }
+    }
+
+    class Program
+        {
+        static void ShowMethod()
+        {
+            Cylinder c = new Cylinder(5.6, 1.6);
+            Console.WriteLine("Surface area :{0}", c.SurfaceArea());
+            Console.WriteLine("Volume is :{0}", _ = c.Volume);
+
+            Console.WriteLine("----------sphere---------");
+            Sphere s = new Sphere(5.6, 1.6);
+            Console.WriteLine("Surface area :{0}", s.SurfaceArea());
+            Console.WriteLine("Volume is :{0}", _ = s.Volume);
+        }
+      
+            static void Main(string[] args)
+            {
+            Program.ShowMethod();
+          
+
+            Console.Read();
+             }
+
+        }
+}
+
